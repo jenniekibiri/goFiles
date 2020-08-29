e51a46f11a31adfcaff2cb5a5804ecc089249e71
jenniekibiri <josphatje@gmail.com>
2020-08-29 18:18:06 +0300

    variable definition definition

---
+++ variables/main.go
@@ -0,0 +1,22 @@
+package main
+import "fmt"
+//use double quotes
+//create the main fun
+func main(){
+	//define variable 
+	//normal method
+	var firstName = "ken"
+	var familyName="charles"
+	//shorthand method
+	age :=12
+	peanutAllergy:=true
+	//print the values
+
+//print the values
+fmt.Println(firstName)
+fmt.Println(familyName)
+fmt.Println(age)
+fmt.Println(peanutAllergy)
+}
+
+
