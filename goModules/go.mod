0b4c8c0d6dd8e5da617ae3b5eb10df9431c0f337
jenniekibiri <josphatje@gmail.com>
2020-11-07 22:29:57 +0300

    install modules using go mod

---
+++ goModules/go.mod
@@ -0,0 +1,10 @@
+module github.com/jenniekibiri/goModules
+
+go 1.14
+
+require (
+	github.com/julienschmidt/httprouter v1.3.0
+	go.uber.org/zap v1.16.0
+	rsc.io/quote v1.5.2
+	rsc.io/quote/v3 v3.1.0
+)
---
+++ goModules/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/julienschmidt/httprouter"
+	"go.uber.org/zap"
+	"rsc.io/quote"
+	quoteV3 "rsc.io/quote/v3"
+
+)
+func main() {
+	router := httprouter.New()
+	router.GET("/", Index)
+
+	log.Fatal(http.ListenAndServe(":8080", router))
+}
+func Index(_ http.ResponseWriter, _ *http.Request, _ httprouter.Params) {
+
+	logger, _ := zap.NewProduction()
+	logger.Info("successfully performed http request")
+	logger.Info(quote.Hello())
+	logger.Info(quoteV3.HelloV3())
+}
