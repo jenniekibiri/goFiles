5798dae19871547ed8ab540fcf7e295642b9722a
jenniekibiri <josphatje@gmail.com>
2023-04-12 13:23:02 +0300

    use printf, println  and variables

--- variables/main.go
+++ variables/main.go
@@ -11,12 +11,23 @@ func main(){
 	age :=12
 	peanutAllergy:=true
 	//print the values
+	//const for not changing values
 
-//print the values
+//print the values format the output with a new line 
 fmt.Println(firstName)
 fmt.Println(familyName)
 fmt.Println(age)
 fmt.Println(peanutAllergy)
+//print does not format the output with a new line 
+//print the values format the output with a new line  
+// the printf doesnt not add a new line you have to add it manual
+fmt.Printf("my name is %v %v and i am %v years old\n",firstName,familyName,age)
+//gettting user input
+ var username string; 
+ fmt.Println("enter your name")
+ //you use the & to get the address of the variable 
+ fmt.Scan(&username)
+ fmt.Println("hello",username)
 }
 
 
