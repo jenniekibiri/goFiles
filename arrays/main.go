f91b0cc3b6903bf1d7123f826b0b603fbd6111c4
jenniekibiri <josphatje@gmail.com>
2023-04-12 22:50:07 +0300

    recap on go routines

--- arrays/main.go
+++ arrays/main.go
@@ -35,7 +35,54 @@ fmt.Println("hello",firstName)
 }
 //for loop
 
+//maps like javacript objects 
+//key value pairs
+var usersMap map[string]string
+usersMap = make(map[string]string)
+usersMap["firstName"] = "ken"
+usersMap["lastName"] = "charles"
+fmt.Printf("users map %v \n",usersMap)
+// or 
+
+// functions  use the 
+add(1,2) // usually outside the main function 
+// you specify the return type after the function name
+
+//conditionals 
+//switch
+switch "firstName" {
+case "ken":
+	fmt.Println("hello ken")
+case "charles":
+	fmt.Println("hello charles")
+default:
+	fmt.Println("hello default")
+}
+
+//structs
+type User struct{
+	FirstName string
+	LastName string
+	Age int
+}
+var userMap2 map[string]User
+userMap2 = make(map[string]User)
+userMap2["ken"] = User{
+	FirstName: "ken",
+	LastName: "charles",
+	Age: 12,
+}
+fmt.Printf("users map %v \n",userMap2)
+// usually a way to create a map with types  of different types
+//concurrency
+//you go  to separate threads
+//async  w.g wait group 
+
+//goroutines
 
 
 
+}
+func add(a int, b int) int{
+	return a+b
 }
